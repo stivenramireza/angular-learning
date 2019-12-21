ab96d9bb20882ebc05b4615db36de76293a5dc6c
Stiven Ramírez Arango <sramirez@anutibara.com>
2019-12-21 11:55:55 -0500

    Adding Data types exercise

---
+++ 1-typescript/app1.js
@@ -0,0 +1,15 @@
+"use strict";
+// Data types in TypeScript
+// String
+var text = "Hello, I'm a text";
+// Number
+var number = 1235;
+// Boolean
+var trueFalse = false;
+// Whatever
+var whatever = "whatever";
+// string[]
+var arrayString = ["text1", "text2"];
+var arrayNumber;
+var arrayBoolean = [true, false];
+var arrayWhatever = ["text", 123, true];
--- 1-typescript/app1.ts
+++ 1-typescript/app1.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+// Data types in TypeScript
+
+// String
+let text:string = "Hello, I'm a text";
+
+// Number
+let number:number = 1235;
+
+// Boolean
+let trueFalse:boolean = false;
+
+// Whatever
+let whatever:any = "whatever"
+
+// string[]
+let arrayString:string[] = ["text1", "text2"];
+let arrayNumber:number[];
+let arrayBoolean:boolean[] = [true, false];
+let arrayWhatever:any[] = ["text", 123, true];
